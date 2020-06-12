8a65f758f293d45892152e4a750071fa157cf9a4
Daniel <dds.daniel@gmail.com>
2020-06-12 16:41:30 -0300

    refatorando a pesquisa de times

--- front-end/Agilis/src/app/components/pessoas/times/times/times.component.ts
+++ front-end/Agilis/src/app/components/pessoas/times/times/times.component.ts
@@ -4,8 +4,6 @@ import { Router } from '@angular/router';
 import { CrudComponent } from 'src/app/components/crud-component';
 import { Time } from 'src/app/models/pessoas/time';
 import { TimesApiService } from 'src/app/services/api/pessoas/times-api.service';
-import { HttpErrorResponse } from '@angular/common/http';
-import { EscopoTime } from 'src/app/enums/pessoas/escopo-time.enum';
 
 @Component({
   selector: 'app-times',
@@ -14,17 +12,11 @@ import { EscopoTime } from 'src/app/enums/pessoas/escopo-time.enum';
 })
 export class TimesComponent extends CrudComponent<Time> {
 
-  // private timesApiService: TimesApiService;
-  // private snackBar: MatSnackBar;
-
   constructor(
     public timesApiService: TimesApiService,
     public snackBar: MatSnackBar,
     router: Router,
   ) {
     super(timesApiService, snackBar, router, 'times');
-
-    // this.timesApiService = timesApiService;
-    // this.snackBar = snackBar;
   }
 }
--- front-end/Agilis/src/app/modules/angular-material/angular-material.module.ts
+++ front-end/Agilis/src/app/modules/angular-material/angular-material.module.ts
@@ -2,21 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
