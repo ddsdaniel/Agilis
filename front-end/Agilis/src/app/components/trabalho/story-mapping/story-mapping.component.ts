7f7a154c78cce19482e56b85d14547bdb919c167
Daniel <dds.daniel@gmail.com>
2020-07-08 19:04:56 -0300

    drag and drop

--- front-end/Agilis/src/app/components/trabalho/story-mapping/story-mapping.component.ts
+++ front-end/Agilis/src/app/components/trabalho/story-mapping/story-mapping.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoryMappingComponent implements OnInit {
 
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
