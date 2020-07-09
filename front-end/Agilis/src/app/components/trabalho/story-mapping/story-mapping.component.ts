ee65487b7ddabd7ddb8b77f7ba3ee52758ad33c1
Daniel <dds.daniel@gmail.com>
2020-07-09 20:46:44 -0300

    tamanhos

--- front-end/Agilis/src/app/components/trabalho/story-mapping/story-mapping.component.ts
+++ front-end/Agilis/src/app/components/trabalho/story-mapping/story-mapping.component.ts
@@ -17,7 +17,7 @@ export class StoryMappingComponent implements OnInit {
           nome: 'Cad. Produtos',
           userStories: [
             {
-              nome: 'Quero cadastrar produto',
+              nome: 'Quero cadastrar um produto genérico, sem preço e que solicite supervisor porque eu sou desconfiado',
             },
             {
               nome: 'Quero alterar o preço',
@@ -49,6 +49,315 @@ export class StoryMappingComponent implements OnInit {
         {
           nome: 'SPED',
           userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Fiscal',
+      epicos: [
+        {
+          nome: 'Apuração de Impostos',
+          userStories: [
+            {
+              nome: 'PIS',
+            },
+            {
+              nome: 'COFINS',
+            },
+            {
+              nome: 'ICMS',
+            }
+          ]
+        },
+        {
+          nome: 'SPED',
+          userStories: [
+            {
+              nome: 'C100',
+            },
+            {
+              nome: 'C170',
+            },
+            {
+              nome: '0200',
+            }
+            ,
+            {
+              nome: 'D100',
+            },
             {
               nome: 'C100',
             },
