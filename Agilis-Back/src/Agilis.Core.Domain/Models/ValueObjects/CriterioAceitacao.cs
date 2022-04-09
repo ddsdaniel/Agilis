6b53ef3e94d46e93eced0cdfc06ee103ef311c39
Daniel <dds.daniel@gmail.com>
2022-04-09 11:37:13 -0300

    CriterioAceitacao

---
+++ Agilis-Back/src/Agilis.Core.Domain/Models/ValueObjects/CriterioAceitacao.cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Validations;
+using Agilis.Core.Domain.Abstractions.Models.ValueObjects;
+
+namespace Agilis.Core.Domain.Models.ValueObjects
+{
+    public class CriterioAceitacao : ValueObject<CriterioAceitacao>
+    {
+        public string Nome { get; private set; }
+        public int Ordem { get; private set; }
+
+        protected CriterioAceitacao() { }
+
+        public CriterioAceitacao(string nome, int ordem)
+        {
+            Nome = nome;
+            Ordem = ordem;
+
+            AddNotifications(new Contract()
+                .IsNotNullOrEmpty(Nome, nameof(Nome), "Nome não deve ser nulo ou vazio")
+                );
+        }
+
+        public override string ToString()
+        {
+            return Nome;
+        }
+    }
+}
+
