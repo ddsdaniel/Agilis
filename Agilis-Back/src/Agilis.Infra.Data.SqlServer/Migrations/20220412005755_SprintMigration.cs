2cccfd744bc74e38593b83717566f9b07185e30b
Daniel <dds.daniel@gmail.com>
2022-04-11 21:58:26 -0300

    migration sprint

---
+++ Agilis-Back/src/Agilis.Infra.Data.SqlServer/Migrations/20220412005755_SprintMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agilis.Infra.Data.SqlServer.Migrations
+{
+    public partial class SprintMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sprints",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Objetivos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataInicial = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DataFinal = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataUltimaAlteracao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sprints", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sprints");
+        }
+    }
+}
