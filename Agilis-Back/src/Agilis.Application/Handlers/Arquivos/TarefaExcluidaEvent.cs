aaa44337f3aa0be98ff1a6efabffd0f34560bb93
Daniel <dds.daniel@gmail.com>
2022-05-09 21:07:02 -0300

    excluindo os arquivos ao excluir uma tarefa

---
+++ Agilis-Back/src/Agilis.Application/Handlers/Arquivos/TarefaExcluidaEvent.cs
@@ -0,0 +1,30 @@
+﻿using Agilis.Core.Domain.Abstractions.UnitsOfWork;
+using Agilis.Core.Domain.Events;
+using Agilis.Core.Domain.Models.Entities;
+using Agilis.Core.Domain.Models.Entities.Tarefas;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Agilis.Application.Handlers.Arquivos
+{
+    public class TarefaExcluidaEvent : INotificationHandler<EntidadeExcluidaDomainEvent<Tarefa>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TarefaExcluidaEvent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(EntidadeExcluidaDomainEvent<Tarefa> notification, CancellationToken cancellationToken)
+        {
+            var arquivoRepository = _unitOfWork.ObterRepository<Arquivo>();
+            foreach (var anexo in notification.Entidade.Anexos)
+            {
+                await arquivoRepository.ExcluirAsync(a => a.Id == anexo.ArquivoId);
+            }
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
